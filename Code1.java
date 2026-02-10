7490a3bfc27be0e61c54c84ddeb5836a91803232
anusha24-shaiva <anushaskanu2410@gmail.com>
2026-02-10 15:44:58 +0530

    Add files via upload

---
+++ Code1.java
@@ -0,0 +1,8 @@
+class Code1{
+public static void main(String [] args){
+    String Str ="Hello";
+String Str1=new String("Hi");
+System.out.println(Str);
+System.out.println(Str1);
+}
+}
---
+++ Code2.java
@@ -0,0 +1,9 @@
+public class Code2 {
+    public static void main(String[] args) {
+        String S1=("Hello");
+        String S2=new String("Hello");
+    System.err.println(S1==S2);
+System.err.println(S1.equals(S2));  
+  }
+    
+}
